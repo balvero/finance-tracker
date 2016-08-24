acfc4c7ce45b4fa87083c2a1c2279a784b9bdd73
Bernard Alvero <bernard.alvero@gmail.com>
2016-08-24 14:08:32 +0800

    Add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my-portfolio', to: 'users#my_portfolio'
   get 'search-stocks', to: 'stocks#search'
+  get 'my-friends', to: 'users#my_friends'
+
 end
